fb3958bea9740000154b85bb730e6383eb1a6a4f
Your Name <your.email@example.com>
2026-04-09 22:16:42 +0300

    init

---
+++ setup.sh
@@ -0,0 +1,9 @@
+
+DOTFILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+tie(){
+    rm -rf "$HOME/.config/$1" && ln -s "$DOTFILES_DIR/$1" ~/.config/$1
+}
+
+tie labwc
+tie foot
